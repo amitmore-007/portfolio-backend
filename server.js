527bbcedb2bb0bcbffdba063d2bca1addfb4f7cd
Amit <amore43035@gmail.com>
2025-03-31 13:57:25 +0530

    Fixed CORS issue

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "https://amit-more-portfolio.netlify.app/", // Allow only your frontend
+  origin: "https://amit-more-portfolio.netlify.app", // Allow only your frontend
   methods: ["POST"],
   allowedHeaders: ["Content-Type"]
 }));
